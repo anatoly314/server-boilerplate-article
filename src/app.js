10f5462af1d8f95a2095b8e8b74b0b4a0308010b
Anatoly Tarnavsky <anatolyt@gmail.com>
2023-02-18 18:56:29 +0200

    Add cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import apiRouter from "./api/api-route.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use('/api', apiRouter);
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
