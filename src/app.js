04b5a28ce928cb2dca971aec97cf2dcf289ed07d
Anatoly Tarnavsky <anatolyt@gmail.com>
2023-02-18 18:48:52 +0200

    Article completed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import apiRouter from "./api/api-route.js";
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 const port = process.env.PORT || 5000;
